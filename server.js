f9eca3506a5f8c5b2c85c6890777f1dc2088210d
afonsoportelaribeiro@gmail.com <tilda21@git.com>
2021-08-16 23:28:14 +0100

    CHanged server js

--- server.js
+++ server.js
@@ -2,22 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 //app.use(requireHTTPS);
-const forceHttps = function() {
-    return function (req, res, next) {
-      if (req.headers['x-forwarded-proto'] !== 'https') {
-          console.log(req.url)
-          console.log(req.get('Host'))
-        return res.redirect(
-         ['https://', req.get('Host'), req.url].join('')
-        );
-      }
-      next();
-    }
-  }
-  // Instruct the app
-  // to use the forceSSL
-  // middleware
-  app.use(forceHttps());
+
 app.use(express.static(path.join(__dirname, '/dist')));
 
 app.get('*', function(req, res) {
